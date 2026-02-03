72f9c71818e92cccae6a6ee7044dc84d474f20b1
Harsh Modi <harshmodi266@gmail.com>
2026-02-03 12:20:03 +0530

    Even_odd.php
    Even,Odd, and Zero number find and 
    print all Even numbers within a specific range.

--- even_odd.php
+++ even_odd.php
@@ -7,4 +7,16 @@ if ($number == 0) {
     }else{
         echo "$number is odd";
     }
+
+?>
+// Print all even numbers within a specific range
+<?php
+$start = 1;
+$end = 20;
+echo = "Evan Number Between $start to $end are = ";
+for($i = $start; $i <= $end; $i++){
+    if($i % 2 == 0){
+        echo $i . " ";
+    }
+}
 ?>
